44e152d9c13f70aac04833a66012f6e82951abf6
alex <alex@pdp7.net>
2025-10-25 19:25:20 +0200

    Duh, move parsing into core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,12 @@
 use extism_pdk::*;
 
 #[plugin_fn]
-pub fn tojson(name: String) -> FnResult<String> {
-    Ok(name)
+pub fn tojson(input: String) -> FnResult<String> {
+    let (header, rest) = input
+        .split_once("\n")
+        .expect("file to have more than one line");
+    match header {
+        "#? json" => Ok(rest.into()),
+        _ => panic!("unknown header {header}"),
+    }
 }
